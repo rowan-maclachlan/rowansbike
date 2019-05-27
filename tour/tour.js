0304c7ff69ae91bbbf375cc10cd172c4b7d75ab6
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2019-05-27 13:23:16 -0600

    Messing around with js trying to get ground moving and drawn

--- tour/tour.js
+++ tour/tour.js
@@ -1,15 +1,22 @@
+import Point from 'Points.js';
 
 const canvas = window.document.getElementById('tour-game');
 
+const MAX_POINTS = 64;
 const center_x = canvas.width / 2;
 const center_y = canvas.height / 2;
 const line_len = 300;
 let angle = 0;
+let points = new Array(MAX_POINTS);
+const START_HEIGHT = canvas.height - 100; 
+/* The distance by which the points move at a time. */
+const DELTA = 10;
+points[0] = new Point(START_HEIGHT, 0);
+points[1] = new Point(START_HEIGHT, canvas.width);
 
 const ctx = canvas.getContext('2d');
 
-drawBackground();
-/* window.setInterval(drawLines, 1); */
+window.setInterval(draw, 1);
 
 function drawSun() {
   ctx.beginPath();
@@ -33,7 +40,7 @@ function drawBackMountains() {
   ctx.lineTo(630, canvas.height - 150);
   ctx.lineTo(canvas.width, canvas.height - 125);
   ctx.lineTo(canvas.width, canvas.height);
-  ctx.lineTo(0, canvas.height);
+  ctx.closePath();
   ctx.fill();
 }
 
@@ -51,7 +58,7 @@ function drawForeMountains() {
   ctx.lineTo(600, canvas.height - 145);
   ctx.lineTo(canvas.width, canvas.height - 110);
   ctx.lineTo(canvas.width, canvas.height);
-  ctx.lineTo(0, canvas.height);
+  ctx.closePath();
   ctx.fill();
 }
 
@@ -61,19 +68,47 @@ function drawBackground() {
   drawForeMountains();
 }
 
-function drawLines() {
-  const da = 1;
-  angle = (angle + da) % 360;
-  let x = (center_x) + (line_len * Math.cos(angle));
-  let y = (center_y) + (line_len * Math.sin(angle));
-  
-  ctx.beginPath(); 
-  ctx.lineWidth = "1";
-  ctx.strokeStyle = "black";
-  ctx.moveTo(center_x, center_y);
-  ctx.lineTo(x, y);
-  ctx.stroke();
+function getDrawablePoints(points) {
+  drawablePoints = points.filter(p => p.x < canvas.width);
+    /* Get the first point off the right side of the screen */
+  lastP = points[points.indexOf(drawablePoints.slice(-1)[0])];
+  return drawablePoints;
 }
 
+function drawGround(drawablePoints) {
+  ctx.fillStyle = "brown";
+  ctx.beginPath();
+  /* start drawing from the first point (offscreen) */
+  ctx.moveTo(drawablePoints[0].x, drawablePoints[0].y);
+  for (p in drawable_points) {
+    ctx.lineTo(p.x, p.y);
+  }
+  ctx.lineTo(last_p.x, last_p.y);
+  /* Draw line to bottom of the screen so we can fill a connected shape */
+  ctx.lineTo(last_p.x, canvas.height);
+  ctx.closePath();
+  ctx.fill();
+}
+
+/* Move points over by some delta on the x axis.
+ * Returns a new list of points. */
+function movePoints(points) {
+  return points.map(p => p.x -= DELTA);
+}
+
+function adjustPoints(points) {
+  /* If the second points is off the screen, delete the first. */
+  if (points[1].x < 0) {
+    points.shift();
+    points.push(new Points(randomX(), randomY()));
+  }
+}
+
+function draw() {
+  drawBackground();
+  drawGround(getDrawablePoints(points));
+  points = movePoints(points);
+  points = adjustPoints(points);
+}
 
 
