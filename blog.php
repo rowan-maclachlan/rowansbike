6a8cdc218523db902db395685eed1bcf58954793
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2019-06-24 14:15:26 -0600

    Added blog content from CouchCMS guide

--- blog.php
+++ blog.php
@@ -1,15 +1,32 @@
 <?php require_once( 'couch/cms.php' ); ?>
-<cms:template title='About Us' />
+<cms:template title='Blog' clonable='1' commentable='1'>
+	<cms:editable name='blog_content' type='richtext' />
+	
+	<cms:editable name='blog_image'
+		crop='1'
+		width='610'
+		height='150'
+		type='image'
+	/>
+	
+	<cms:folder name="philosophy" title="Philosophy" />
+	<cms:folder name="history" title="History" />
+	<cms:folder name="jobs" title="Jobs" />
+	<cms:folder name="staff" title="Staff" />
+	<cms:folder name="clients" title="Clients" />
+	
+</cms:template>
+
+<cms:if k_is_page >
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Aurelius | About Us</title>
+	<title>rowansbike | blog</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
 	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/reset.css" />
-	<link rel="stylesheet" href="css/styles.css" />
-	
+	<link rel="stylesheet" href="main.css" />
+
 	<!-- Scripts -->
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 	<!--[if IE 6]>
@@ -31,78 +48,101 @@
 	<div id="wrapper" class="container_12 clearfix">
 
 		<!-- Text Logo -->
-		<h1 id="logo" class="grid_4">Aurelius</h1>
+		<h1 id="logo" class="grid_4">Blog</h1>
 		
 		<!-- Navigation Menu -->
 		<ul id="navigation" class="grid_8">
 			<li><a href="contact.html"><span class="meta">Get in touch</span><br />Contact Us</a></li>
-			<li><a href="blog.html"><span class="meta">Latest news</span><br />Blog</a></li>
+			<li><a href="blog.html" class="current"><span class="meta">Latest news</span><br />Blog</a></li>
 			<li><a href="portfolio.html"><span class="meta">Our latest work</span><br />Portfolio</a></li>
-			<li><a href="about.html" class="current"><span class="meta">Who are we?</span><br />About</a></li>
+			<li><a href="about.html"><span class="meta">Who are we?</span><br />About</a></li>
 			<li><a href="index.html"><span class="meta">Homepage</span><br />Home</a></li>
 		</ul>
 		
 		<div class="hr grid_12 clearfix">&nbsp;</div>
 			
 		<!-- Caption Line -->
-		<h2 class="grid_12 caption">Learn <span>about us</span> and what we do best.</h2>
+		<h2 class="grid_12 caption clearfix">Our <span>blog</span>, keeping you up-to-date on our latest news.</h2>
 		
 		<div class="hr grid_12 clearfix">&nbsp;</div>
-
-		<!-- Column 1 / Content -->
-		<cms:editable name='main_content' type='richtext'>
+		
+		<!-- Column 1 /Content -->
 		<div class="grid_8">
-			<h4 class="page_title">Our mission</h4>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</i></p> 
-			<h4 class="page_title">So who are we?</h4>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> 
-			<h4 class="page_title">Staff</h4>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> 
-			<h4 class="page_title">Clients</h4>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor. Phasellus cursus augue ac purus. Curabitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> 
 			
-		</div>
-		</cms:editable>
-		
-		<!-- Column 2 / Sidebar -->
-		<cms:editable name='sidebar_content' type='richtext'>
-		<div class="grid_4">
-		
-			<h4>Our History</h4>
+			<!-- Blog Post -->
+			<div class="post">
+				<!-- Post Title -->
+				<h3 class="title"><a href="<cms:show k_page_link />"><cms:show k_page_title /></a></h3>
+				<!-- Post Title -->
+				<cms:if k_page_foldertitle >
+					<cms:set my_category=k_page_foldertitle />
+				<cms:else />
+					<cms:set my_category='Uncategorised' />
+				</cms:if>
+				<p class="sub"><a href="#"><cms:show my_category /></a> &bull; <cms:date k_page_date format='jS M, y'/> &bull; <a href="#"><cms:show k_comments_count /> Comments</a></p>
+				<div class="hr dotted clearfix">&nbsp;</div>
+				<!-- Post Title -->
+				<img class="thumb" src="<cms:show blog_image />" alt=""/>
+				<!-- Post Content -->
+				<cms:show blog_content />
+				<!-- Post Links -->
+				<p class="clearfix">
+					<a href="<cms:link masterpage='blog.php' />" class="button float" >&lt;&lt; Back to Blog</a>
+					<a href="#commentform" class="button float right" >Discuss this post</a>
+				</p>
+			</div>
+			<div class="hr clearfix">&nbsp;</div>
+			
+			<!-- Comment's List -->
+			<h3>Comments</h3>
 			<div class="hr dotted clearfix">&nbsp;</div>
-			<dl class="history"> 
-				<dt>1994</dt> 
-				<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor.</dd> 
 			
-				<dt>1996</dt> 
-				<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor.</dd> 
+			<cms:if k_comments_count >
+			<ol class="commentlist">
+				<cms:comments page_id=k_page_id order='asc' limit='5' paginate='1' >
+				<li class="comment"> 
+					<div class="gravatar"> 
+						<cms:gravatar email="<cms:show k_comment_author_email />" size="60" />
+						<a name="<cms:show k_comment_anchor />">
+					</div> 
+					<div class="comment_content"> 
+						<div class="clearfix">
+							<cite class="author_name"><a href=""><cms:show k_comment_author /></a></cite>       
+							<div class="comment-meta commentmetadata"><cms:date k_comment_date format='F j, Y'/> at <cms:date k_comment_date format='h:ia'/></a></div> 
+						</div>
+						<div class="comment_text"> 
+							<p><cms:show k_comment /></p> 
+						</div> 
+					</div> 
+				</li> 
+				<cms:paginator />
+				</cms:comments>
+			</ol> 
+			<cms:else />
+				No comments.
+			</cms:if>
 			
-				<dt>2000</dt> 
-				<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor.</dd> 
+			<div class="hr clearfix">&nbsp;</div>
 			
-				<dt>2003</dt> 
-				<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor.</dd> 
-			
-				<dt>2009</dt> 
-				<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel diam id mauris accumsan egestas. Sed sed lorem. Integer id mi vel sapien fermentum vehicula. Pellentesque vitae lacus a sem posuere fringilla. Vestibulum dolor.</dd> 
-			</dl>
-		</div>
-		</cms:editable>
+			<!-- Comment Form -->
+			<cms:embed 'comments_form.html' />
 
-		<div class="hr grid_12 clearfix">&nbsp;</div>
+		</div>
+	
+		<!-- Column 2 / Sidebar -->
+		<cms:embed 'blog_sidebar.html' />
 		
 		<!-- Footer -->
 		<p class="grid_12 footer clearfix">
 			<span class="float"><b>&copy; Copyright</b> <a href="">QwibbleDesigns</a> - remove upon purchase.</span>
 			<a class="float right" href="#">top</a>
 		</p>
-		
 	</div><!--end wrapper-->
 
 </body>
 </html>
+<cms:else />
+	<cms:embed 'blog_list.html' />
+</cms:if>
+
 <?php COUCH::invoke(); ?>
