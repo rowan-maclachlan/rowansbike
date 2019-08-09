e91e044e3dd158310fdeefcbe63fef5215ec5830
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2019-08-09 16:30:54 -0700

    Added header snippet

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<nav>
+  <a id="logo" href="/index.php">
+    <span style="font-family:'Liberty Island'">ROWAN'S BIKE</span>
+  </a>
+  <a class="bike-nav" href="/bikes/khs-alite.php">KHS Alite 100</a>
+  <a class="bike-nav" href="/bikes/cambio-rino.php">Cambio Rino</a>
+  <a class="bike-nav" href="/bikes/beekay-mirage.php">Beekay Mirage</a>
+  <a class="bike-nav" href="/bikes/nova.php">Nova</a>
+  <a class="bike-nav" href="/bikes/bianchi-volpe.php">Bianchi Volpe</a>
+  <a href="/blog">Blog</a>
+  <a id="tour" class="bike-nav" href="/tour">The Tour</a>
+</nav>
